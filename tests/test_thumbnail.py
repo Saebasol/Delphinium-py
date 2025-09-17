d01afd67b1e97c3c6bc64df11e1afc40d9c420c6
Ryu Juheon <saidbysolo@gmail.com>
2025-09-18 03:02:44 +0900

    feat: image and thumbnail handling to use ResolvedImage (#20)

--- tests/test_thumbnail.py
+++ tests/test_thumbnail.py
@@ -1,49 +1,56 @@
 import pytest
 
 from delphinium.client import Delphinium
+from delphinium.entities.file import File
 
 
 @pytest.mark.asyncio
 async def test_thumbnail_single(client: Delphinium):
     thumbnail = await client.thumbnail(1, "smallsmall")
-    assert thumbnail[0].startswith("https://")
+    assert thumbnail[0].url.startswith("https://")
     assert len(thumbnail) == 1
+    assert isinstance(thumbnail[0].file, File)
 
 
 @pytest.mark.asyncio
 async def test_thumbnail_multi(client: Delphinium):
     thumbnail = await client.thumbnail(1, "smallsmall", single=False)
     assert len(thumbnail) >= 1
-    assert all(url.startswith("https://") for url in thumbnail)
+    assert all(image.url.startswith("https://") for image in thumbnail)
+    assert isinstance(thumbnail[0].file, File)
 
 
 @pytest.mark.asyncio
 async def test_thumbnail_smallsmall_size(client: Delphinium):
     thumbnail = await client.thumbnail(1, "smallsmall")
-    assert thumbnail[0].startswith("https://")
+    assert thumbnail[0].url.startswith("https://")
     assert len(thumbnail) == 1
-    assert "smallsmall" in thumbnail[0]
+    assert "smallsmall" in thumbnail[0].url
+    assert isinstance(thumbnail[0].file, File)
 
 
 @pytest.mark.asyncio
 async def test_thumbnail_small_size(client: Delphinium):
     thumbnail = await client.thumbnail(1, "small")
-    assert thumbnail[0].startswith("https://")
+    assert thumbnail[0].url.startswith("https://")
     assert len(thumbnail) == 1
-    assert "small" in thumbnail[0]
+    assert "small" in thumbnail[0].url
+    assert isinstance(thumbnail[0].file, File)
 
 
 @pytest.mark.asyncio
 async def test_thumbnail_smallbig_size(client: Delphinium):
     thumbnail = await client.thumbnail(1, "smallbig")
-    assert thumbnail[0].startswith("https://")
+    assert thumbnail[0].url.startswith("https://")
     assert len(thumbnail) == 1
-    assert "smallbig" in thumbnail[0]
+    assert "smallbig" in thumbnail[0].url
+    assert isinstance(thumbnail[0].file, File)
 
 
 @pytest.mark.asyncio
 async def test_thumbnail_big_size(client: Delphinium):
     thumbnail = await client.thumbnail(1, "big")
-    assert thumbnail[0].startswith("https://")
+    assert thumbnail[0].url.startswith("https://")
     assert len(thumbnail) == 1
-    assert "big" in thumbnail[0]
+    assert "big" in thumbnail[0].url
+    assert isinstance(thumbnail[0].file, File)
